697405125444ffdd4d61076c4c89d9c26904bc6e
Adam Nogowski <adam.nogowski1@ge.com>
2024-03-29 22:44:18 -0600

    handled time for results name added extensions.json updated settings.json

--- src/pytest_loop.py
+++ src/pytest_loop.py
@@ -9,21 +9,20 @@ import re
 from pluggy import HookspecMarker
 from _pytest.main import Session
 from _pytest.config import Config
-from _pytest.logging import LogCaptureFixture
 
-
-SECONDS_IN_HOUR : float = 3600
-SECONDS_IN_MINUTE : float = 60
-SHORTEST_AMOUNT_OF_TIME : float = 0
+SECONDS_IN_HOUR: float = 3600
+SECONDS_IN_MINUTE: float = 60
+SHORTEST_AMOUNT_OF_TIME: float = 0
 hookspec = HookspecMarker("pytest")
+
 
 class InvalidTimeParameterError(Exception):
 	pass
 
-def pytest_configure(config : Config):
-	config.addinivalue_line(
-		'markers',
-		'loop(n): run the given test function `n` times.')
+
+def pytest_configure(config: Config):
+	config.addinivalue_line('markers', 'loop(n): run the given test function `n` times.')
+
 
 def pytest_addoption(parser):
 	"""
@@ -31,40 +30,40 @@ def pytest_addoption(parser):
 	"""
 	pytest_loop = parser.getgroup("loop")
 	pytest_loop.addoption(
-		"--delay",
-		action="store",
-		default=0,
-		type=float,
-		help="The amount of time to wait between each test loop.",
+	    "--delay",
+	    action="store",
+	    default=0,
+	    type=float,
+	    help="The amount of time to wait between each test loop.",
 	)
 	pytest_loop.addoption(
-		"--hours",
-		action="store",
-		default=0,
-		type=float,
-		help="The number of hours to loop the tests for.",
+	    "--hours",
+	    action="store",
+	    default=0,
+	    type=float,
+	    help="The number of hours to loop the tests for.",
 	)
 	pytest_loop.addoption(
-		"--minutes",
-		action="store",
-		default=0,
-		type=float,
-		help="The number of minutes to loop the tests for.",
+	    "--minutes",
+	    action="store",
+	    default=0,
+	    type=float,
+	    help="The number of minutes to loop the tests for.",
 	)
 	pytest_loop.addoption(
-		"--seconds",
-		action="store",
-		default=0,
-		type=float,
-		help="The number of seconds to loop the tests for.",
+	    "--seconds",
+	    action="store",
+	    default=0,
+	    type=float,
+	    help="The number of seconds to loop the tests for.",
 	)
-	
+
 	pytest_loop.addoption(
-		'--loop',
-		action='store',
-		default=1,
-		type=int,
-		help='The number of times to loop each test',
+	    '--loop',
+	    action='store',
+	    default=1,
+	    type=int,
+	    help='The number of times to loop each test',
 	)
 
 
@@ -74,32 +73,30 @@ def pytest_runtestloop(session: Session) -> bool:
 	Reimplement the test loop but loop for the user defined amount of time or iterations.
 	"""
 	if session.testsfailed and not session.config.option.continue_on_collection_errors:
-		raise session.Interrupted(
-			"%d error%s during collection"
-			% (session.testsfailed, "s" if session.testsfailed != 1 else "")
-		)
+		raise session.Interrupted("%d error%s during collection" % (session.testsfailed, "s" if session.testsfailed != 1 else ""))
 
 	if session.config.option.collectonly:
 		return True
-		
-	start_time : float = time.time()
-	count : int = 0
+
+	start_time: float = time.time()
+	total_time: float = 0
+	count: int = 0
 	iterations = session.config.option.loop
 	m = session.get_closest_marker('loop')
 
 	if m is not None:
 		iterations = int(m.args[0])
-		
 
-	while count <= iterations: # Need to run atleast once
+	while total_time >= SHORTEST_AMOUNT_OF_TIME or count <= iterations:  # need to run at least one for normal tests
 		count += 1
-		if _get_total_time(session) or count <= iterations:
-			_print_loop_count(count, iterations,)
-		
+		total_time = _get_total_time(session)
+
+		_print_loop_count(count, iterations)
 		for index, item in enumerate(session.items):
-			item : pytest.Item = item
-			item._report_sections.clear() #clear reports for new test
-			if iterations > 1:
+			item: pytest.Item = item
+			item._report_sections.clear()  #clear reports for new test
+
+			if total_time > SHORTEST_AMOUNT_OF_TIME or count < iterations:
 				pattern = " - run\[\d+\]"
 				if re.search(pattern, item._nodeid):
 					new_str = f" - run[{count}]"
@@ -107,19 +104,19 @@ def pytest_runtestloop(session: Session) -> bool:
 				else:
 					item._nodeid = item._nodeid + f" - run[{count}]"
 
-			next_item : pytest.Item = session.items[index + 1] if index + 1 < len(session.items) else None
+			next_item: pytest.Item = session.items[index + 1] if index + 1 < len(session.items) else None
 			item.config.hook.pytest_runtest_protocol(item=item, nextitem=next_item)
 			if session.shouldfail:
 				raise session.Failed(session.shouldfail)
 			if session.shouldstop:
 				raise session.Interrupted(session.shouldstop)
 		if _timed_out(session, start_time) and count >= iterations:
-			break # exit loop
+			break  # exit loop
 		time.sleep(_get_delay_time(session))
 	return True
 
 
-def _get_delay_time(session : Session):
+def _get_delay_time(session: Session):
 	"""
 	Helper function to extract the delay time from the session.
 
@@ -129,7 +126,7 @@ def _get_delay_time(session : Session):
 	return session.config.option.delay
 
 
-def _get_total_time(session : Session) -> float:
+def _get_total_time(session: Session) -> float:
 	"""
 	Takes all the user available time options, adds them and returns it in seconds.
 
@@ -145,7 +142,7 @@ def _get_total_time(session : Session) -> float:
 	return total_time
 
 
-def _timed_out(session : Session, start_time : float) -> bool:
+def _timed_out(session: Session, start_time: float) -> bool:
 	"""
 	Helper function to check if the user specified amount of time has lapsed.
 
@@ -154,7 +151,8 @@ def _timed_out(session : Session, start_time : float) -> bool:
 	"""
 	return time.time() - start_time > _get_total_time(session)
 
-def _print_loop_count(count : int, iterations: int):
+
+def _print_loop_count(count: int, iterations: int):
 	"""
 	Helper function to simply print out what loop number we're on.
 
@@ -163,5 +161,8 @@ def _print_loop_count(count : int, iterations: int):
 	"""
 	column_length = shutil.get_terminal_size().columns
 	print("\n")
-	print(f" Loop # {count}/{iterations} ".center(column_length, "="))
+	if iterations > 1:
+		print(f" Loop # {count}/{iterations} ".center(column_length, "="))
+	else:
+		print(f" Loop # {count} ".center(column_length, "="))
 	print("\n")
